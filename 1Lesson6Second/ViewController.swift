ee05ffe686383413f70724358eb7dc681902de08
sirojiddinbakhtiyorovich <msirojiddin62@gmail.com>
2023-07-09 19:40:32 +0500

    Initial Commit

---
+++ 1Lesson6Second/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1Lesson6Second
+//
+//  Created by Sirojiddin Bakhtiyorovich on 21.12.1444 (AH).
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
